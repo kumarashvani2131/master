b73040eadc8f995b7cf1f552c15059bd9890a771
ASHVANI KUMAR <ashvani.kumar@innoeye.com>
2021-03-04 14:21:14 +0530

    adding java file

---
+++ StudentRegister/StudentRegister.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+class StudentRegister
+{
+
+	public Student register(Student student){
+		ArrayList<Student> al = new ArrayList();
+		return al.add("101","ashvani",234);
+	
+	}
+}
